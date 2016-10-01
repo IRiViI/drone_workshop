0d7732777ad3faaa7b0c999320cf3ada3dbe13bd
IRiViI <rckvnk@gmail.com>
2016-10-01 14:10:13 +0200

    working on peer connection

--- drone/js/peer_connection.js
+++ drone/js/peer_connection.js
@@ -1,23 +1,43 @@
 function PeerConnection(root,connection,ice_servers){
 
-	pc = window.RTCPeerConnection(ice_servers)
+	pc = window.RTCPeerConnection(ice_servers);
+
+	var pcSettings = _getPCSettings(root.id,connection);
+	console.log(pcSettings);
 
 	//var pc = RTCPeerConnection(ice_servers);
 	pc.root = root;
-	pc.drone_id = connection.drone_id;
+	pc.drone_id = settings.drone_id;
 	pc.hive_id = connection.hive_id;
-	pc.settings = {connection:this.audio, video:connection.video};
+	pc.settings = {
+		offerToReceiveAudio:connection.offerToReceiveAudio, 
+		offerToReceiveVideo:connection.offerToReceiveVideo, 
+		audio:settings.audio, 
+		video:settings.video};
 	pc.onicecandidate = _onicecandidate;
 	pc.connect = _connect;
 	return pc;
 
 }
 
+function _getPCSettings(drone_id,connection){
+	var t_drone_settings = connection.drone.length;
+	for (var i_drone_settings = 0; i_drone_settings < t_drone_settings; i_drone_settings++){
+		var drone_settings = connection.drone[i_drone_settings];
+		if (drone_settings.drone_id == drone_id){
+			return drone_settings
+		}
+	}
+	return null;
+}
+
+
 function _onicecandidate(event){
 	if (event.candidate != null){
 	    if (!event || !event.candidate) {
 	      return;
 	    }
+	    // Send request
 	    this.root.websocket_client.sendRequest(
 	      	{tag:"candidate",
 	      	drone_id:root.id,
